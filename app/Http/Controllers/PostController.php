f37d7e736db04d2c8f5d26b131cec3d117ee22f2
clovis <clovis-@live.com>
2017-03-27 16:53:06 -0400

    WIP - Added first page integration

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,11 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
-{
-    
+{    
     public function generateBlog($blog-id){
     	$post = post::where('id','=',$blog-id);
     	return view('post',$post);
     	];
     }
-}0
+}
