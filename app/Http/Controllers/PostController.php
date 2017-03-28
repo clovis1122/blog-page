82ba7cedf4568f033f5e82f8a14bd06ff07ead53
clovis <clovis-@live.com>
2017-03-28 11:37:45 -0400

    typo

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{    
+    public function generateBlog($blog-id){
+    	$post = post::where('id','=',$blog-id);
+    	return view('post',$post);
+    	];
+    }
+}
