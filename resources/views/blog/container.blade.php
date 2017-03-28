14a6b3f0b10238fa8e8b1c67f565845633316a8c
clovis <clovis-@live.com>
2017-03-28 10:53:56 -0400

    Migrate from PHP7.1 to  PHP5.6

---
+++ resources/views/blog/container.blade.php
@@ -0,0 +1,10 @@
+    <div class="container">
+
+      @yield('blog')
+      @yield('blog')
+      @yield('blog')
+      @yield('blog')
+      @yield('blog')
+      @yield('blog')
+      
+    </div>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My Personal blog</title>
+    </head>
+
+    <body>
+    
+        <div class='blog-container'>
+            @yield('container')
+        </div>
+
+    </body>
+</html>
